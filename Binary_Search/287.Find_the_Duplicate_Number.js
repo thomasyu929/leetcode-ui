76c20adcbbacb619bfae0c3ed3eb6666a1fed14b
Thomas Yu <yby4301955@gmail.com>
2021-11-06 18:24:49 -0500

    add binary solution for 287

--- Binary_Search/287.Find_the_Duplicate_Number.js
+++ Binary_Search/287.Find_the_Duplicate_Number.js
@@ -5,25 +5,47 @@
 
 // https://leetcode.com/problems/find-the-duplicate-number/
 
-var findDuplicate = function(nums) {
-  let slow = 0,
-      fast = 0;
-  while (true) {
-    slow = nums[slow];
-    fast = nums[nums[fast]];
+// var findDuplicate = function(nums) {
+//   let slow = 0,
+//       fast = 0;
+//   while (true) {
+//     slow = nums[slow];
+//     fast = nums[nums[fast]];
     
-    if (slow === fast) {
-      break;
+//     if (slow === fast) {
+//       break;
+//     }
+//   }
+  
+//   fast = 0;
+//   while (true) {
+//     slow = nums[slow];
+//     fast = nums[fast];
+    
+//     if (slow === fast) {
+//       return fast;
+//     }
+//   }
+// };
+
+var findDuplicate = function(nums) {
+  let left = 1,
+      right = nums.length;
+  while (left < right) {
+    let mid = Math.floor((right - left) / 2) + left;
+    let count = 0;
+    for (let num of nums) {
+      if (num <= mid) {
+        count++;
+      }
+    }
+    if (count > mid) {
+      right = mid;
+    }
+    else {
+      left = mid + 1;
     }
   }
   
-  fast = 0;
-  while (true) {
-    slow = nums[slow];
-    fast = nums[fast];
-    
-    if (slow === fast) {
-      return fast;
-    }
-  }
-};
+  return right;
+}
