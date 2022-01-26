54c15bbc7da6ef72aeb415b17e5c0fbad4fc55e1
Thomas Yu <yby4301955@gmail.com>
2022-01-25 23:57:06 -0600

    2

---
+++ LinkedList/2.Add_Two_Number.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+// https://leetcode.com/problems/add-two-numbers/
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+ var addTwoNumbers = function(l1, l2) {
+  let dummy = new ListNode(null);
+  let curr = dummy;
+  let carry = 0;
+  
+  while (l1 || l2) {
+    let val1 = l1 ? l1.val : 0;
+    let val2 = l2 ? l2.val : 0;
+    let sum = val1 + val2 + carry;
+    carry = Math.floor(sum / 10);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+  if (carry) curr.next = new ListNode(1);
+  
+  return dummy.next;
+};
